820c729cbf3de0fdfb04643a8908a439716b1964
anh.tran5dev <anh.tran5@apac.merkleinc.com>
2023-07-06 23:54:31 +0700

    init repo

--- medusa-config.js
+++ medusa-config.js
@@ -33,6 +33,13 @@ const DATABASE_URL =
 
 const REDIS_URL = process.env.REDIS_URL || "redis://localhost:6379";
 
+const GoogleClientId = process.env.GOOGLE_CLIENT_ID || "670556147701-8pndkplrgdivhej6oa5vgn9ruckbneq4.apps.googleusercontent.com"
+const GoogleClientSecret = process.env.GOOGLE_CLIENT_SECRET || "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"
+const BACKEND_URL = process.env.BACKEND_URL || "localhost:9000"
+const ADMIN_URL = process.env.ADMIN_URL || "localhost:7000"
+const STORE_URL = process.env.STORE_URL || "localhost:8000"
+
+
 const plugins = [
   `medusa-fulfillment-manual`,
   `medusa-payment-manual`,
@@ -42,6 +49,48 @@ const plugins = [
       upload_dir: "uploads",
     },
   },
+  {
+    resolve: "medusa-plugin-auth",
+    options: {
+        // strict: "all", // or "none" or "store" or "admin"
+        google: {
+            clientID: GoogleClientId,
+            clientSecret: GoogleClientSecret,
+ 
+            admin: {
+                callbackUrl:`${BACKEND_URL}/admin/auth/google/cb?scope=email`,
+                failureRedirect: `${ADMIN_URL}/login`,
+ 
+				// The success redirect can be overriden from the client by adding a query param `?redirectTo=your_url` to the auth url
+				// This query param will have the priority over this configuration
+                successRedirect: `${ADMIN_URL}/`,
+ 
+                authPath: '/admin/auth/google?scope=email',
+                // authCallbackPath: '/admin/auth/google/cb',
+                // expiresIn: 24 * 60 * 60 * 1000,
+                // verifyCallback: (container, req, accessToken, refreshToken, profile) => {
+                //    // implement your custom verify callback here if you need it
+                // }
+            },
+ 
+            store: {
+                callbackUrl:`${BACKEND_URL}/store/auth/google/cb`,
+                failureRedirect: `${STORE_URL}/login`,
+ 
+				// The success redirect can be overriden from the client by adding a query param `?redirectTo=your_url` to the auth url
+				// This query param will have the priority over this configuration
+                successRedirect: `${STORE_URL}/`,
+ 
+                // authPath: '/store/auth/google',
+                // authCallbackPath: '/store/auth/google/cb',
+                // expiresIn: 24 * 60 * 60 * 1000,
+                // verifyCallback: (container, req, accessToken, refreshToken, profile) => {
+                //    // implement your custom verify callback here if you need it
+                // }
+            }
+        }
+    }
+  }
   // To enable the admin plugin, uncomment the following lines and run `yarn add @medusajs/admin`
   // {
   //   resolve: "@medusajs/admin",
